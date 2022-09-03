c8456f93fb8ceccc4252b8f2de3da7e2beab532a
Bongo9911 <dawsonbuist@gmail.com>
2022-09-03 13:50:46 -0400

    fixing b.points

--- commands/points.js
+++ commands/points.js
@@ -8,25 +8,30 @@ exports.run = async (bot, message, args) => {
 
     let nonZeroItems = gameData.items.filter(m => m.points > 0);
 
-    let columns = Math.ceil(nonZeroItems.length / 25);
-    let perColumn = Math.ceil(nonZeroItems.length / columns);
+    if (nonZeroItems.length && gameData.active) {
+        let columns = Math.ceil(nonZeroItems.length / 25);
+        let perColumn = Math.ceil(nonZeroItems.length / columns);
 
-    const pointsEmbed = new MessageEmbed()
-        .setColor('#0099ff');
+        const pointsEmbed = new MessageEmbed()
+            .setColor('#0099ff');
 
-    for (let i = 0; i < columns; ++i) {
-        let pointCol = "";
-        for (let j = i * perColumn; j < (i + 1) * perColumn && j < nonZeroItems.length; ++j) {
-            pointCol += "(" + nonZeroItems[j].label + ") " + (nonZeroItems[j].emoji ? nonZeroItems[j].emoji + " " : "") + nonZeroItems[j].item + " - **" + nonZeroItems[j].points + "**\n";
+        for (let i = 0; i < columns; ++i) {
+            let pointCol = "";
+            for (let j = i * perColumn; j < (i + 1) * perColumn && j < nonZeroItems.length; ++j) {
+                pointCol += "(" + nonZeroItems[j].label + ") " + (nonZeroItems[j].emoji ? nonZeroItems[j].emoji + " " : "") + nonZeroItems[j].item + " - **" + nonZeroItems[j].points + "**\n";
+            }
+            if (i == 0) {
+                pointsEmbed.addField("Points", pointCol, true);
+            }
+            else {
+                pointsEmbed.addField("\u200b", pointCol, true);
+            }
         }
-        if (i == 0) {
-            pointsEmbed.addField("Points", pointCol, true);
-        }
-        else {
-            pointsEmbed.addField("\u200b", pointCol, true);
-        }
+        message.reply({ embeds: [pointsEmbed] });
     }
-    message.reply({ embeds: [pointsEmbed] });
+    else {
+        message.reply("There is currently no active game.");
+    }
 }
 
 exports.help = {
